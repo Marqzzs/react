b11e142d42bd7f3cc7d2834a64489a2273737ac1
Marqzzs <maumarqz007@gmail.com>
2023-11-08 10:51:25 -0300

    a

--- eventplus/src/Pages/HomePage/HomePage.jsx
+++ eventplus/src/Pages/HomePage/HomePage.jsx
@@ -3,17 +3,37 @@ import "./Home.css";
 import Banner from "../../Components/Banner/Banner";
 import MainContent from "../../Components/Main/MainContent";
 import VisionSection from "../../Components/VisionSection/VisionSection";
+import ContactSection from "../../Components/ContactSection/ContactSection";
+import Title from "../../Components/Title/Title";
+import NextEvent from "../../Components/NextEvent/NextEvent";
+import Container from "../../Components/Container/Container";
 
 const HomePage = () => {
   return (
-    
-      <MainContent>
+    <MainContent>
+      <Banner />
 
-        <Banner />
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Proximos Eventos"} />
 
-        <VisionSection />
+          <div className="events-box">
+            <NextEvent 
+              title={"Event X"}
+              description={"Evento legal"}
+              eventDate={"16/11/2023"}
+              idEvent={"1"}
+            />
+            <NextEvent />
+            <NextEvent />
+          </div>
+        </Container>
+      </section>
 
-      </MainContent>
+      <VisionSection />
+
+      <ContactSection />
+    </MainContent>
   );
 };
 
